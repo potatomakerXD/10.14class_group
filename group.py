a4ed6c3e538b69cddebbaad17348e86b98952900
Armin <rmapjg0@ucl.ac.uk>
2024-10-14 19:01:13 +0100

    Define data structures and insert data

--- group.py
+++ group.py
@@ -2,4 +2,19 @@
 
 # Your code to go here...
 
-my_group =
+# Create the initial group structure
+my_group = [
+    {"name": "Jill", "age": 26, "job": "Biologist", "connections": [{"name": "Zalika", "relationship": "friend"},{"name": "John", "relationship": "partner"}]},
+    {"name": "Zalika", "age": 28, "job": "Artist", "connections": [{"name": "Jill", "relationship": "friend"}]},
+    {"name": "John", "age": 27, "job": "Writer", "connections": [{"name": "Jill", "relationship": "partner"}]},
+    {"name": "Nash", "age": 34, "job": "Chef", "connections": [{"name": "John", "relationship": "cousin"},{"name": "Zalika", "relationship": "landlord"}]}
+]
+group = my_group
+
+# Print the final group structure in a nicely formatted way
+for i in group:
+    print(i)
+    
+    
+
+
